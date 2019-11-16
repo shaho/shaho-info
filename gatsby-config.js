c1e614c29ece4640b6145fb41f4de2f80d89378e
shaho <toofani@gmail.com>
2019-11-16 18:33:07 +0330

    Add basic structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,35 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+const path = require("path");
 
 module.exports = {
-  /* Your site config here */
-}
+  plugins: [
+    "gatsby-plugin-sass",
+
+    {
+      resolve: "gatsby-plugin-postcss",
+      options: {
+        postCssPlugins: [
+          require("tailwindcss"),
+          require("./tailwind.config.js"),
+          require("autoprefixer"),
+        ],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-alias-imports",
+      options: {
+        alias: {
+          "@src": path.join(__dirname, "src"),
+          "@components": path.join(__dirname, "src/components"),
+          "@layouts": path.join(__dirname, "src/components/layouts"),
+          "@pages": path.join(__dirname, "src/pages"),
+          "@styles": path.join(__dirname, "src/styles"),
+          "@static": path.join(__dirname, "static"),
+          "@store": path.join(__dirname, "src/store"),
+          "@providers": path.join(__dirname, "src/components/providers"),
+          "@common": path.join(__dirname, "src/components/common"),
+          "@containers": path.join(__dirname, "src/components/containers"),
+        },
+      },
+    },
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,41 @@
-import React from "react"
+import React from "react";
 
-export default () => <div>Hello world!</div>
+// ─── STYLES ─────────────────────────────────────────────────────────────────────
+import "@styles/tailwind.css";
+import "@styles/global.scss";
+
+export default () => {
+  return (
+    <div className="bg-gray-100 h-screen">
+      <div id="inner" className="px-8 py-12">
+        <img
+          src="https://i.postimg.cc/yNRpZV4Y/logo.jpg"
+          alt=""
+          className="h-10"
+        />
+        <img
+          src="https://i.postimg.cc/jjX4yB17/img.jpg"
+          alt=""
+          className="mt-6 rounded-lg shadow-xl"
+        />
+        <h1 className="mt-6 text-2xl font-bold text-gray-900 leading-tight">
+          You can work from anywhere.
+          <span className="text-indigo-500">Take advantage of it.</span>
+        </h1>
+        <p className="mt-2 text-gray-600">
+          Workcation helps you find work-friendly rentals in beautiful locations
+          so you can enjoy some nice weather even when you're not on vaction.
+        </p>
+        <div className="mt-4">
+          <a
+            href="#top"
+            className="inline-block px-5 py-3 shadow-lg rounded-lg bg-indigo-500 text-sm text-white uppercase tracking-wider font-semibold"
+          >
+            Book your escape
+          </a>
+        </div>
+      </div>
+      {/*─── END INNER ───────────────────────────────────────────────────*/}
+    </div>
+  );
+};
